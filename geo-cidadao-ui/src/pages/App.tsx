bd4e4a46c033ac9d84fab595841001b1c4b906be
Pedro <pedro_henrique_dsr@hotmail.com>
2025-09-18 21:19:52 -0300

    commit teste

--- geo-cidadao-ui/src/pages/App.tsx
+++ geo-cidadao-ui/src/pages/App.tsx
@@ -18,7 +18,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR teste
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
