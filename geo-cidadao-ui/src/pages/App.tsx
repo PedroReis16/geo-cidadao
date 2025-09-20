0e14d31b8be9b82d67a10f89883bab6cb3aed70d
Pedro <pedro_henrique_dsr@hotmail.com>
2025-09-19 21:20:15 -0300

    feat: Roteamento de páginas com definição de rota default

--- geo-cidadao-ui/src/pages/App.tsx
+++ geo-cidadao-ui/src/pages/App.tsx
@@ -1,24 +1,55 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
 
 //Components
 import Header from "../ui/components/Header";
 
 //Pages
 import HomePage from "./HomePage";
+import PostPage from "./PostPage";
+import NotFoundPage from "./NotFoundPage";
 
 // Styles
 import "../ui/styles/App.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Outlet />,
+    children: [
+      {
+        index: true,
+        path: "feed",
+        element: <HomePage />,
+      },
+      {
+        path: "posts/:id",
+        element: <PostPage />,
+      },
+      {
+        path: "*",
+        element: <NotFoundPage />,
+      },
+      {
+        index: true,
+        element: <Navigate to="/feed" replace />, // Redirect root to /feed
+      },
+    ],
+  },
+]);
+
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Header />
       <main>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-        </Routes>
+        <RouterProvider router={router} />
       </main>
-    </BrowserRouter>
+    </>
   );
 }
 
