0c431cf5e6a5b67d2a9dcc6bd816c353522af60a
Pedro <pedro_henrique_dsr@hotmail.com>
2025-10-06 21:18:53 -0300

    feat: Pacote para manipulação do mongoDB

--- resources/geo-cidadao-services/geocidadaodotnet/GeoCidadao.Model/Enums/ErrorCodes.cs
+++ resources/geo-cidadao-services/geocidadaodotnet/GeoCidadao.Model/Enums/ErrorCodes.cs
@@ -3,6 +3,11 @@ namespace GeoCidadao.Model.Enums
     public enum ErrorCodes
     {
 
+        VALIDATION_ERROR = 1,
+        ENTITY_NOT_FOUND = 2,
+
+
+        
         UNKNOWN_ERROR = 999,
     }
 }
