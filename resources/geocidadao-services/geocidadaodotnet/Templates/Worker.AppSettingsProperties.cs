381673f1b6abacda74715abd24ea0b37e4b6fb44
Pedro Henrique <pedro_henrique_dsr@hotmail.com>
2025-11-20 12:16:54 -0300

    chore: Script para criação de projetos worker

---
+++ resources/geocidadao-services/geocidadaodotnet/Templates/Worker.AppSettingsProperties.cs
@@ -0,0 +1,7 @@
+namespace <PROJECT_NAME>.Config;
+
+public class AppSettingsProperties
+{
+    public const string MaxConcurrency = "MaxConcurrency";
+    public const string RabbitMQ = "RabbitMQ";
+}
