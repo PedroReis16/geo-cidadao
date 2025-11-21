fd65533ece6e2e5b5123fb22114f9e644542f6a7
Pedro Henrique <pedro_henrique_dsr@hotmail.com>
2025-11-20 23:36:00 -0300

    refactor: Atualização do formato de inicialização do post indexer para o formato esperado

---
+++ resources/geocidadao-services/geocidadaodotnet/WorkerServices/GeoCidadao.PostIndexerWorker/Config/AppSettingsProperties.cs
@@ -0,0 +1,9 @@
+namespace GeoCidadao.PostIndexerWorker.Config
+{
+    public class AppSettingsProperties
+    {
+        public const string MaxConcurrency = "MaxConcurrency";
+        public const string RabbitMQ = "RabbitMQ";
+
+    }
+}
--- resources/geocidadao-services/geocidadaodotnet/WorkerServices/GeoCidadao.PostIndexerWorker/Program.cs
+++ resources/geocidadao-services/geocidadaodotnet/WorkerServices/GeoCidadao.PostIndexerWorker/Program.cs
@@ -1,7 +1,87 @@
-using GeoCidadao.PostIndexerWorker;
+using GeoCidadao.PostIndexerWorker.Contracts.QueueServices;
+using GeoCidadao.PostIndexerWorker.Services.QueueServices;
+using GeoCidadao.Models.Config;
+using Quartz;
+using GeoCidadao.Jobs.Config;
+using GeoCidadao.Jobs.Listeners;
+using GeoCidadao.PostIndexerWorker.Config;
+using GeoCidadao.PostIndexerWorker.Jobs;
+
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+
+builder.Services.ConfigureServiceLogs();
+
+// Queue Services
+builder.Services.AddSingleton<INewPostQueueService, NewPostQueueService>();
+builder.Services.AddSingleton<IPostInterectedQueueService, PostInteractedQueueService>();
+builder.Services.AddSingleton<IPostDeletedQueueService, PostDeletedQueueService>();
+
+builder.Services.Configure<QuartzOptions>(options =>
+{
+    options.Scheduling.IgnoreDuplicates = true;
+    options.Scheduling.OverWriteExistingData = false;
+});
+
+builder.Services.AddQuartz(q =>
+{
+    q.SchedulerId = q.SchedulerName = JobConstants.SchedulerName;
+
+    q.AddJobListener<JobChainListener>();
+
+    q.UseSimpleTypeLoader();
+    q.UseInMemoryStore();
+    q.UseDefaultThreadPool(tp =>
+    {
+        int maxConcurrency = builder.Configuration
+                      .GetRequiredSection(AppSettingsProperties.MaxConcurrency)
+                      .Get<int>();
+
+        tp.MaxConcurrency = maxConcurrency > 0 ? maxConcurrency : Environment.ProcessorCount;
+    });
+
+    // Fila para processar novos posts
+    q.AddJob<NewPostQueueJob>(j =>
+    {
+        j.WithIdentity(nameof(NewPostQueueJob));
+    });
+
+    q.AddTrigger(t =>
+    {
+        t.ForJob(nameof(NewPostQueueJob));
+        t.StartNow();
+    });
+
+    // Fila para processar posts alterados
+    q.AddJob<PostChangedQueueJob>(j =>
+    {
+        j.WithIdentity(nameof(PostChangedQueueJob));
+    });
+
+    q.AddTrigger(t =>
+    {
+        t.ForJob(nameof(PostChangedQueueJob));
+        t.StartNow();
+    });
+
+    // Fila para processar posts alterados
+    q.AddJob<PostDeletedQueueJob>(j =>
+    {
+        j.WithIdentity(nameof(PostDeletedQueueJob));
+    });
+
+    q.AddTrigger(t =>
+    {
+        t.ForJob(nameof(PostDeletedQueueJob));
+        t.StartNow();
+    });
+});
+
+builder.Services.AddQuartzHostedService(options =>
+{
+    options.WaitForJobsToComplete = true;
+});
+
 
 var host = builder.Build();
 host.Run();
