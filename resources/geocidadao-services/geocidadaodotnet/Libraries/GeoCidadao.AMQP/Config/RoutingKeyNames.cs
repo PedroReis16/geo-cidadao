d51bfccb6c5b9e33a9554cbf29058022853af80a
Pedro Henrique <pedro_henrique_dsr@hotmail.com>
2025-11-20 23:35:07 -0300

    feat: Configuração das propriedades destinadas a indexação dos posts

--- resources/geocidadao-services/geocidadaodotnet/Libraries/GeoCidadao.AMQP/Config/RoutingKeyNames.cs
+++ resources/geocidadao-services/geocidadaodotnet/Libraries/GeoCidadao.AMQP/Config/RoutingKeyNames.cs
@@ -32,22 +32,32 @@ public static class RoutingKeyNames
     public const string DLQ_POST_CHANGED_ROUTING_KEY = "post.changed.dlq";
 
     /// <summary>
-    /// Routing key para notificação de interação em posts (curtidas e comentários)
+    /// Routing key para notificação de novos posts
     /// </summary>
-    public const string POST_INTERACTION_ROUTING_KEY = "post.interaction";
+    public const string NEW_POST_ROUTING_KEY = "post.new";
 
     /// <summary>
-    /// Dead Letter Routing key para notificação de interação em posts
+    /// Dead Letter Routing key para notificação de novos posts
     /// </summary>
-    public const string DLQ_POST_INTERACTION_ROUTING_KEY = "post.interaction.dlq";
+    public const string DLQ_NEW_POST_ROUTING_KEY = "post.new.dlq";
 
     /// <summary>
-    /// Routing key para notificação de eventos de analytics de posts com localização
+    /// Routing key para notificação de interações em posts
     /// </summary>
-    public const string POST_ANALYTICS_ROUTING_KEY = "post.analytics";
+    public const string POST_INTERACTED_ROUTING_KEY = "post.interacted";
 
     /// <summary>
-    /// Dead Letter Routing key para notificação de eventos de analytics de posts
+    /// Dead Letter Routing key para notificação de interações em posts
     /// </summary>
-    public const string DLQ_POST_ANALYTICS_ROUTING_KEY = "post.analytics.dlq";
+    public const string DLQ_POST_INTERACTED_ROUTING_KEY = "post.interacted.dlq";
+
+    /// <summary>
+    /// Routing key para notificação de posts deletados
+    /// </summary>
+    public const string POST_DELETED_ROUTING_KEY = "post.deleted";
+
+    /// <summary>
+    /// Dead Letter Routing key para notificação de posts deletados
+    /// </summary>
+    public const string DLQ_POST_DELETED_ROUTING_KEY = "post.deleted.dlq";
 }
