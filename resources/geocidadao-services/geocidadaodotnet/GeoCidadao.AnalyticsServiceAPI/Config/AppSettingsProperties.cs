99df2fb951c1f0f1711842e542568a4a615f20cc
Pedro Henrique <pedro_henrique_dsr@hotmail.com>
2025-11-22 12:06:48 -0300

    chore: Remoção de projetos que não estavam sendo utilizados

--- resources/geocidadao-services/geocidadaodotnet/GeoCidadao.AnalyticsServiceAPI/Config/AppSettingsProperties.cs
+++
@@ -1,9 +0,0 @@
-namespace GeoCidadao.AnalyticsServiceAPI.Config;
-
-public class AppSettingsProperties
-{
-    public const string OAuth = "OAuth";
-    public const string PortalAuthClient = "PortalAuthClient";
-    public const string MaxConcurrency = "MaxConcurrency";
-    public const string RabbitMQ = "RabbitMQ";
-}
