57f66c526b454b12531f9d2fc8deb97be905f322
Pedro Henrique <pedro_henrique_dsr@hotmail.com>
2025-11-24 01:35:30 -0300

    feat: Referencia das APIs de autenticação uteis para o feed

--- resources/geocidadao-services/geocidadaodotnet/GeoCidadao.FeedServiceAPI/Config/AppSettingsProperties.cs
+++ resources/geocidadao-services/geocidadaodotnet/GeoCidadao.FeedServiceAPI/Config/AppSettingsProperties.cs
@@ -6,4 +6,13 @@ public class AppSettingsProperties
     public const string PortalAuthClient = "PortalAuthClient";
     public const string MaxConcurrency = "MaxConcurrency";
     public const string RabbitMQ = "RabbitMQ";
+    public const string ApiUrls = "ApiUrls";
+    public const string UserManagementClient = "UserManagementClient";
+    public const string GerenciamentoUsuariosAPI = "GerenciamentoUsuariosAPI";
+    public const string EngagementClient = "EngagementClient";
+    public const string EngagementServiceAPI = "EngagementServiceAPI";
+    public const string Keycloak = "Keycloak";
+    public const string KeycloakAdmin = "KeycloakAdmin";
+    public const string TokenClient = "keycloakToken";
+
 }
